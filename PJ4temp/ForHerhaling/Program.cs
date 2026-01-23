5f98a4ccdeaee5a29d6b7c45b77647fd4c297e99
unknown <applecowws@gmail.com>
2026-01-23 15:37:33 +0100

    TEMP4H3

---
+++ PJ4temp/ForHerhaling/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace PJ4temp;
+
+class Program
+{
+
+    internal class Formulier
+    {
+        internal int Sterren {get; set;}
+        internal string Feedback {get; set;}
+
+        public override string ToString()
+        {
+            return Sterren  + Feedback;
+        }
+
+    } 
+    static void Main(string[] args)
+    {
+        double[] prijzen = new double[] {0.99, 5.60, 10.11};
+        string[] artiekelen = new string[] {"candy", "drugs", "help me"};
+        Formulier[] _formulier = new Formulier[2];
+        _formulier[0] = new Formulier()
+        {
+            Sterren = 10,
+            Feedback = "expensiiive"
+        };
+        _formulier[1] = new Formulier()
+        {
+          Sterren = 67,
+          Feedback = "hollow hungy"  
+        };
+
+        for (int i = 0; i < _formulier.Length; i++)
+        {
+            Console.WriteLine($"{_formulier[i].Sterren} {_formulier[i].Feedback}");
+        } //vraag
+
+
+         for (int i = 0; i < prijzen.Length; i++)
+        {
+            Console.WriteLine(prijzen[i]);
+        }
+    foreach (Formulier formulier in _formulier)
+    {
+        Console.WriteLine(formulier);
+    }
+    }
+}
---
+++ PJ4temp/Lists/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.Intrinsics.Wasm;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Lists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> charList = new List<string>()
+     {
+        "Pac-Man"
+        ,"Samus"
+        ,"Crash Bandicoot"
+        ,"Spyro the Dragon"
+        ,"Donkey Kong"
+        ,"Mario"
+        ,"Luigi"
+        ,"Astro"
+        ,"Solid Snake"
+        ,"Master Chief"
+        ,"Kratos"
+        ,"Sonic the Hedgehog"
+        ,"Link"
+       };
+
+        for (int i = 0; i < charList.Count; i++)
+        {
+            Console.WriteLine(charList[i]);
+        }
+        charList.Remove("Pac-Man");
+
+        List<double> reviews = new List<double>()
+        {
+            123
+            ,123123
+            ,1231
+            ,23
+            ,12
+
+        };
+        reviews.Remove(123);
+        reviews.RemoveAt(0);
+        for (int i = 0; i < reviews.Count; i++)
+        {
+            Console.WriteLine(reviews[i]);
+        }
+
+           List<Pickup> pick = new List<Pickup>();
+
+
+    }
+
+    
+        
+}
+
